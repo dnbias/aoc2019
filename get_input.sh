0159b587454bcdb23385388c5b32ffc5d3f986ce
Daniel Biasiotto <daniel.biasiotto@edu.unito.it>
2025-11-01 02:54:46 +0100

    feat: better input curling

--- get_input.sh
+++ get_input.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-read -p "Enter your session cookie: " cookie
+cookie_file="$1"
+cookie=$(cat "$cookie_file" 2> /dev/null)
+echo cookie: $cookie
+part_num=""
+part=""
+
+if [ "$cookie" == "" ]; then
+  read -p "Enter your session cookie: " cookie
+fi
 read -p "Enter the day number: " day
+read -p "Enter the day part (leave blank for first part): " part_num
 
-mkdir "day$day"
+mkdir "day$day" 2> /dev/null
 
-url="https://adventofcode.com/2019/day/$day"
+if [ "$part_num" != "" ] ; then
+    part="#part$part_num"
+fi
 
-curl $url -o "day$day/input" --cookie "session=$cookie"
+url="https://adventofcode.com/2019/day/$day$part/input"
+echo "contacting $url..."
+
+curl -s "$url"  --cookie "session=$cookie" >> day$day/input$part_num
+
+echo "written response to day$day/input$part_num"
+
+bat "day$day/input$part_num"
